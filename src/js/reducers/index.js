d434713026bdbf9eaf387b85f304cf1582818944
Kristofer Jarl <kristofer.jarl@pagero.com>
2017-09-09 12:39:18 +0200

    Toolbox-reducer in place

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux'
 import firebase from './firebase-reducer'
 import users from './user-reducer'
-import fakeReducer from './fake-reducer'
+import toolbox from './toolbox-reducer'
 import parts from './drawing-board-reducer'
 
 export default combineReducers({
     firebase,
     users,
-    fakeReducer,
+    toolbox,
     parts
 })
---
+++ src/js/reducers/toolbox-reducer.js
@@ -0,0 +1,7 @@
+const initialState = {}
+
+const toolbox = function(state = initialState, action) {
+    return state
+}
+
+export default toolbox
