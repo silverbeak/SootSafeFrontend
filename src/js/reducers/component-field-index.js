87f608bd9250eee6edce295974e3b02af8219ec9
Kristofer Jarl <kristofer.jarl@pagero.com>
2017-10-24 18:52:48 +0200

    More fields for parts

--- src/js/reducers/component-field-index.js
+++ src/js/reducers/component-field-index.js
@@ -49,7 +49,10 @@ const base = {
                 unit: 'mm'
             }
         }
-    },
+    }
+}
+
+const capacity = {
     capacity: {
         type: Number,
         value: 1,
@@ -59,25 +62,35 @@ const base = {
     }
 }
 
+const pressureLoss = {
+    pressureLoss: {
+        type: Number,
+        value: 0,
+        min: 0,
+        path: 'fields.pressureLoss',
+        unit: 'Pa'
+    }
+}
+
 const mergeSingle = component => {
     switch(component.key) {
         case 1: 
-        return Object.assign({}, component, { type: AVAILABLE_TYPES.outlet }, { fields: base })
+        return Object.assign({}, component, { type: AVAILABLE_TYPES.outlet }, { fields: Object.assign({}, base, pressureLoss) })
         case 3:
-        return Object.assign({}, component, { type: AVAILABLE_TYPES.firecell })
+        return Object.assign({}, component, { type: AVAILABLE_TYPES.firecell }, { fields: Object.assign({}, base) })
         case 5: 
         case 7:
-        return Object.assign({}, component, { type: AVAILABLE_TYPES.pipe })
+        return Object.assign({}, component, { type: AVAILABLE_TYPES.pipe }, { fields: Object.assign({}, base, capacity, pressureLoss) })
         case 11:
         case 12:
         case 13:
         case 14:
-        return Object.assign({}, component, { type: AVAILABLE_TYPES.bend })
+        return Object.assign({}, component, { type: AVAILABLE_TYPES.bend }, { fields: Object.assign({}, base, capacity, pressureLoss) })
         case 21:
         case 22:
         case 23:
         case 24:
-        return Object.assign({}, component, { type: AVAILABLE_TYPES.tpipe})
+        return Object.assign({}, component, { type: AVAILABLE_TYPES.tpipe}, { fields: Object.assign({}, base, capacity, pressureLoss) })
         default:
         return component
     }
