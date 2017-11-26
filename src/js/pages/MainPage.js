20918f34eadb6b26e6837c96894456de68a737f7
Kristofer Jarl <kristofer.jarl@pagero.com>
2017-11-26 19:23:29 +0100

    Rudimentary starting page, to say the least :)

---
+++ src/js/pages/MainPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class MainPage extends React.Component {
+    render () {
+        return (
+            <h2>Welcome to SootSafe</h2>
+        )
+    }
+}
+
+export default MainPage
