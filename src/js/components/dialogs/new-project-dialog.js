5d0f946494215c158573aad334b05eb5f52aa2e9
Kristofer Jarl <kristofer.jarl@pagero.com>
2018-05-15 19:43:04 +0200

    Converting project dialog and project menu to latest material-ui

--- src/js/components/dialogs/new-project-dialog.js
+++ src/js/components/dialogs/new-project-dialog.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import Button from 'material-ui/Button'
-import Dialog, {
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle
-} from 'material-ui/Dialog'
-import TextField from 'material-ui/TextField'
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import TextField from '@material-ui/core/TextField'
 import { createNewProject, createNewSketch } from '../../actions/backend-communicator-actions'
 import * as dialogActions from '../../actions/dialog-actions'
 
