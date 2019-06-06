90ebe1df2a19b59ef4603a3a1ba1975f666352b7
Kristofer Jarl <kristofer.jarl@trollmoj.com>
2019-06-06 16:22:47 +0200

    Fleshed out user profile

--- src/js/pages/user/UserSettings.jsx
+++ src/js/pages/user/UserSettings.jsx
@@ -1,13 +1,82 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/styles'
+import * as _ from 'lodash'
+
+function mapStateToProps(state) {
+    return {
+        user: state.users.user
+    };
+}
+
+const styles = {
+    profile: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    leftBar: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '10em',
+        padding: '1.5em',
+    },
+    rightBar: {
+        display: 'flex',
+        flex: 1,
+        padding: '1.5em',
+        marginLeft: '2em',
+    },
+    displayName: {
+        color: 'gray',
+    },
+    profilePhoto: {
+        borderRadius: '10px'
+    },
+}
 
 class UserSettings extends Component {
+
+    findProviderDataField = (user, fieldName) => {
+        const data = _.find(user.providerData, pd => !!pd[fieldName])
+        return _.get(data, fieldName)
+    }
+
     render() {
+        const { user, classes } = this.props
+
+        if (!user) return <div>Loading user data...</div>
+
+        // console.log('User', user);
+        
+
+        const photoUrl = this.findProviderDataField(user, 'photoURL')
+        const displayName = this.findProviderDataField(user, 'displayName')
+        const email = this.findProviderDataField(user, 'email')
+        
         return (
-            <div>
-                User settings coming soon
+            <div className={classes.profile}>
+                <div className={classes.leftBar}>
+                    {
+                        photoUrl ? 
+                            <img src={photoUrl} style={{ height: '10em' }} className={classes.profilePhoto} />
+                            :
+                            <div>No photo to display</div>
+                    }
+
+                    <h4 className={classes.displayName}>{displayName}</h4>
+                    <b className={classes.displayName}><a href={`mailto:${email}`}>{email}</a></b>
+
+                </div>
+
+                <div className={classes.rightBar}>
+                    <p>User info coming here...</p>
+                </div>
+
             </div>
         );
     }
 }
 
-export default UserSettings;
+export default connect(
+    mapStateToProps,
+)(withStyles(styles)(UserSettings))
